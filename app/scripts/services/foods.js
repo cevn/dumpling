8b4751f7e7cf6ad33358bc970d1da22dcbb233e5
Sameer Dhar <avielus@gmail.com>
2014-04-13 15:36:25 -0400

    Tried to fix some dependency injection errors

--- app/scripts/services/foods.js
+++ app/scripts/services/foods.js
@@ -1,11 +1,11 @@
 'use strict';
 
 angular.module('dumplingApp')
-  .factory('foods', function($resource) {
+  .factory('foods', ['$resource', function($resource) {
     return $resource('assets/foods.json', {}, {
       query: {
         method: 'GET',
         isArray: true
       }
     });
-  });
+  }]);
